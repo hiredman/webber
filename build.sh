b445974c97f1568029836ef444a98973bf9d707d
Kevin Downey <redchin@gmail.com>
2011-07-25 19:20:36 -0700

    change bin name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cat webber.m \
-    | gcc -framework Cocoa -framework WebKit -x objective-c -o myweb - \
-    && ./myweb
+    | gcc -framework Cocoa -framework WebKit -x objective-c -o webber - \
+    && ./webber
